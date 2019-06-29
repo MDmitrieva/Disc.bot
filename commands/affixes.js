95d764dea6a8383458d35e624526ab67b14acd48
Maria <zenitka008@mail.ru>
2019-06-29 21:00:51 +0300

    embedding affixes command

--- commands/affixes.js
+++ commands/affixes.js
@@ -1,29 +1,26 @@
 const discord = require("discord.js");
 const request = require('request-promise')
 
-
 module.exports.run = (bot, message, args,) => {
    return request.get("https://raider.io/api/v1/mythic-plus/affixes?region=eu&locale=ru")
     .then (response => {
         console.log(response)
-
         let res = JSON.parse(response)
         console.log(res)
-     
         let details = res.affix_details
-    
-        let result = ""
+        let embed = {
+            title: "**Аффиксы на текущее КД:**",
+            color: 3447003,
+            thumbnail:{
+                "url": "https://s.tcdn.co/4dd/300/4dd300fd-0a89-3f3d-ac53-8ec93976495e/5.png"},
+            fields: [],
+        };
         details.forEach(element => {
-        result+=`**${element.name}**\n`
-        result+=element.description+"\n"
-        result+="\n"
-            
-        });
-        return { message:result, files: []}
+            embed.fields.push({name: element.name, value: element.description})
+             });
+        message.channel.send({embed});
     })
 }
-
-
 module.exports.help = {
     name: "аффиксы"
 }
--- commands/embed.js
+++ commands/embed.js
@@ -1,35 +1,64 @@
 const discord = require("discord.js");
 const request = require('request-promise')
-
-
-module.exports.run = (bot, message, args,) => {
-   return request.get("https://raider.io/api/v1/mythic-plus/affixes?region=eu&locale=ru")
+const RuNames = require('./RuNames.json')
+const underscore = require("underscore");
+const _ = require("underscore");
+   
+module.exports.run = (bot, message, parameters,) => {
+    let character = parameters[0]; //тут мы ожидаем что юзер рилм вписал итд
+    let realm = parameters[1];
+        if(RuNames[realm] !== undefined) realm = RuNames[realm]
+        if(realm == null) {realm = "howlingfjord"}
+    let url = `https://raider.io/api/v1/characters/profile?region=eu&realm=${realm}&name=${encodeURI(character)}&fields=mythic_plus_scores%2Cmythic_plus_highest_level_runs%2Cmythic_plus_weekly_highest_level_runs`;
+    console.log(url)
+   return request.get(`https://raider.io/api/v1/characters/profile?region=eu&realm=${realm}&name=${encodeURI(character)}&fields=mythic_plus_scores%2Cmythic_plus_highest_level_runs%2Cmythic_plus_weekly_highest_level_runs`)
     .then (response => {
-        console.log(response)
+         console.log(response) 
         let res = JSON.parse(response)
         console.log(res)
-        let details = res.affix_details
-        let embed = {
-            title: "**Аффиксы на текущее КД:**",
-            color: 3447003,
-            thumbnail:{
-                "url": "https://cdn.shopify.com/s/files/1/1061/1924/products/Smiling_Devil_Emoji_large.png?v=1480481056"},
-            author: {
-                "name": "Николаев",
-                "icon_url": "https://cdn.discordapp.com/app-icons/503896289721974785/93df0c03d3c3b4a2c1882a78a57eb67c.png"
-            },
-            fields: []
-
-        };
-        details.forEach(element => {
-            embed.fields.push({name: element.name, value: element.description})
+        var key = res
+            var name = key.name
+            var mps = key.mythic_plus_scores.all
+            var week = key.mythic_plus_weekly_highest_level_runs
+            var best = key.mythic_plus_highest_level_runs
+            let sortedRuns = _.sortBy(week, 'score');
+            var weekBest = _.last(sortedRuns)
+            let bestSortedRuns = _.sortBy(best, 'score');
+            var seasonBest = _.last(bestSortedRuns)
+            var photo = key.thumbnail_url
+       let embed =  {
+           title: `**${name}**`,
+           color: 3447003,
+           thumbnail: photo,
+           fields: [
+               {name: "**RIO:**", 
+               value:`${mps}`,
+               inline: true},
+               {name: `**Лучший ключ на неделе:**`,
+               value: `${RuNames[weekBest.dungeon]} +${weekBest.mythic_level}`,
+                if(week.length == 0) {name=`**Ключ на этом КД не пройден!**\n`}}
+           ]
+};    
+    
+           
+      
+        if(week.length == 0) {result+=`**Ключ на этом КД не пройден!**\n`}
+        else 
+        { 
+                result+=`**Лучший ключ на неделе:** ${RuNames[weekBest.dungeon]} +${weekBest.mythic_level}\n`
+                    if(weekBest.num_keystone_upgrades == 0) {result+=`Кто-то слоупок и не успел вовремя \n`}
+                    else 
+                    {result+=`Ключ улучшен на + ${weekBest.num_keystone_upgrades}\n`}
             
-        });
-        return { message, files: [embed]}
+        }
+        if(best.length == 0) {result+=`**Да он(а) в этом сезоне никуда и не ходил(а)!**\n`}
+        else{
+            result+=`**Лучший ключ в сезоне** ${RuNames[seasonBest.dungeon]} +${seasonBest.mythic_level}\n`
+            result+=`Ключ улучшен на + ${seasonBest.num_keystone_upgrades}\n` 
+        }
+        message.channel.send({embed});
     })
 }
-
-
 module.exports.help = {
     name: "test"
 }
